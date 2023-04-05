cc420f27dd4e18f2a486af12f2dd4b6ec63df790
Lars van der Laan <vanderlaanlars@yahoo.com>
2023-04-05 15:57:28 -0700

    done

--- simScripts/simsAdapt.sh
+++ simScripts/simsAdapt.sh
@@ -5,8 +5,10 @@ export R_LIBS_USER=~/Rlibs2
 for n in 500 1000 2000 3000 4000 5000
 do
   for const in 1 2 3
-    for hard in true false
   do
+    for hard in true false
+    do
     sbatch  --export=n=$n,const=$const, hard=$hard, ~/causalHAL/simScriptAdapt.sbatch
+    done
   done
 done
