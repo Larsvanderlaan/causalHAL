0a06735451dc8d2d2a8cb1df03cfedb6130b7793
Lars van der Laan <vanderlaanlars@yahoo.com>
2023-04-05 15:49:51 -0700

    nodes

--- simScripts/install_packages.R
+++ simScripts/install_packages.R
@@ -1,5 +1,5 @@
 devtools::install_github("tlverse/hal9001", ref = "screeningHAL")
 devtools::install_github("tlverse/sl3", ref = "develVersionChangeLars")
 devtools::install_cran("doMC")
-devtools::install_github("Larsvanderlaan/causalHAL", force = T)
+devtools::install_github("Larsvanderlaan/causalHAL")
 
