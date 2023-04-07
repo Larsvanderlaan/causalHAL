82dd3d2923eb6a6290894efa7262818f488f0c35
Lars van der Laan <vanderlaanlars@yahoo.com>
2023-04-07 10:30:36 -0700

    hey

--- simScripts/simScriptAdapt.R
+++ simScripts/simScriptAdapt.R
@@ -3,9 +3,11 @@ library(hal9001)
 library(sl3)
 library(causalHAL)
 library(doMC)
-doMC::registerDoMC(cores = 6)
+doMC::registerDoMC(cores = 11)
+
 do_sims <- function(niter, n, pos_const, muIsHard, do_local_alt = FALSE) {
   sim_results <- rbindlist(lapply(1:niter, function(iter) {
+    print(paste0("Iteration number: ", iter))
     try({
     data_list <- get_data(n, pos_const, muIsHard)
     return(as.data.table(get_estimates(data_list$W, data_list$A, data_list$Y,iter)))
@@ -48,10 +50,10 @@ get_data_local_alt <- function(n, pos_const, muIsHard = TRUE) {
     mu0 <-  W[,1] + W[,2]  + W[,4] + W[,5]
   }
   # Add local alternative fluctuation
-  mu0 <- mu0 + (W[,3] + W[,4] + W[,6] + W[,7])/sqrt(n)
+  mu0 <- mu0 + (1 + W[,3] + W[,4] + W[,6] + W[,7])/sqrt(n)
   tau <- 1 + W[,1] + W[,3] + W[,5]
   # Add local alternative fluctuation
-  tau <- (W[,2] + W[,4] + W[,6] + W[,7])/sqrt(n)
+  tau <- tau + (1 + W[,2] + W[,4] + W[,6] + W[,7])/sqrt(n)
   Y <- rnorm(n,  mu0 + A * tau, 0.5)
   return(list(W=W, A = A, Y = Y, ATE = 1))
 }
@@ -61,13 +63,13 @@ get_data_local_alt <- function(n, pos_const, muIsHard = TRUE) {
 get_estimates <- function(W, A, Y,iter) {
   n <- length(Y)
   if(n <= 500) {
-    num_knots <- 25
+    num_knots <- c(25, 5)
   } else if(n <= 1000) {
-    num_knots <- 50
+    num_knots <- c(50, 15)
   } else{
-    num_knots <- 100
+    num_knots <- c(100, 30)
   }
-  fit_T <- fit_hal_cate(W, A, Y,  max_degree = 1, num_knots = c(num_knots), smoothness_orders = 1,max_degree_cate =1, num_knots_cate = num_knots, smoothness_orders_cate = 1,    screen_variables = TRUE, fit_control = list(parallel = TRUE))
+  fit_T <- fit_hal_cate(W, A, Y,  max_degree = 1, num_knots = num_knots, smoothness_orders = 1,max_degree_cate =1, num_knots_cate = num_knots, smoothness_orders_cate = 1,    screen_variables = TRUE, fit_control = list(parallel = TRUE))
   ate_T <- unlist(inference_cate(fit_T))
   ate_T[1] <- "Tlearner"
 
@@ -78,12 +80,22 @@ get_estimates <- function(W, A, Y,iter) {
   ate_R[1] <- "Rlearner"
 
   lrnr_A <- fit_R$internal$fit_EAW
-  fit_R_intercept <- fit_hal_pcate(W, A, Y, lrnr_Y = NULL, lrnr_A = lrnr_A,  max_degree =2, num_knots = num_knots,  max_degree_cate = 1, num_knots_cate = 2, smoothness_orders_cate = 1,    screen_variables = TRUE, formula_cate = ~ h(W7), fit_control = list(parallel = TRUE))
-  mean(fit_R$internal$data$tau_relaxed)
-  ate_intercept <-  unlist(inference_cate(fit_R_intercept))
-  ate_intercept[1] <- "intercept"
+  #fit_R_intercept <- fit_hal_pcate(W, A, Y, lrnr_Y = NULL, lrnr_A = lrnr_A,  max_degree =2, num_knots = num_knots,  max_degree_cate = 1, num_knots_cate = 2, smoothness_orders_cate = 1,    screen_variables = TRUE, formula_cate = ~ h(W7), fit_control = list(parallel = TRUE))
+  #mean(fit_R$internal$data$tau_relaxed)
+  #ate_intercept <-  unlist(inference_cate(fit_R_intercept))
+  #ate_intercept[1] <- "intercept"
 
   pi <- fit_R$internal$data$pi
+  m <- fit_R$internal$data$mu
+  tau_int <- mean((A-pi) * (Y - m)) / mean((A-pi)^2)
+  IF <- (A - pi) / mean((A-pi)^2) * (Y - m - (A-pi)*tau_int)
+  CI <- tau_int + 1.96*c(-1,1)*sd(IF)/sqrt(n)
+  ate_intercept <- c("intercept", tau_int, sd(IF)/sqrt(n), CI)
+  names(ate_intercept) <- c("method", "coef","se", "CI_left", "CI_right")
+
+
+
+
   mu1 <- fit_T$internal$data$mu1
   mu0 <- fit_T$internal$data$mu1
   mu <- ifelse(A==1, mu1, mu0)
