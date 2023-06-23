cf201d420a66bacf8c478ebde6634f364dc1b515
Lars van der Laan <vanderlaanlars@yahoo.com>
2023-06-23 10:23:04 -0700

    up

--- R/truncate_pscore_adaptive.R
+++ R/truncate_pscore_adaptive.R
@@ -3,16 +3,18 @@
 #' @export
 #'
 truncate_pscore_adaptive <- function(A, pi, min_trunc_level = 1e-8) {
-  cutoffs <- seq(0.1, min_trunc_level, length = 500)
-  risks <- sapply(cutoffs, function(cutoff) {
-    pi <- pmin(pi, 1 - cutoff)
-    pi <- pmax(pi, cutoff)
+  risk_function <- function(cutoff, level) {
+     pi <- pmax(pi, cutoff)
+     pi <- pmin(pi, 1 - cutoff)
     alpha <- A/pi - (1-A)/(1-pi) #Riesz-representor
     alpha1 <- 1/pi
     alpha0 <- - 1/(1-pi)
     mean(alpha^2 - 2*(alpha1 - alpha0))
-  })
-  cutoff <- cutoffs[which.min(risks)[1]]
+  }
+  cutoff <- optim(1e-5, fn = risk_function, method = "Brent", lower = min_trunc_level, upper = 0.5, level = 1)$par
   pi <- pmin(pi, 1 - cutoff)
   pi <- pmax(pi, cutoff)
+  pi
 }
+
+
