8e31133f63d8f7054f55b67a6bd489e042eab7ae
Lars van der Laan <vanderlaanlars@yahoo.com>
2023-07-02 17:24:22 -0700

    hey

---
+++ simScripts/simScriptDR.sh
@@ -0,0 +1,11 @@
+#!/bin/usr/env bash
+nsims=2500
+export R_LIBS=~/Rlibs2
+export R_LIBS_USER=~/Rlibs2
+for n in 250 500 1000 2000 3000 4000 5000
+do
+  for const in 0.5 1 2 3
+    do
+    sbatch  --export=n=$n,const=$const ~/causalHAL/simScripts/simScriptDR.sbatch
+  done
+done
